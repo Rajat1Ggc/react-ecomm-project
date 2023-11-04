d4ae2637f901747fd40b9ba951c3a49e05479b87
Rajat1ggc <Rajat1ggc@gmail.com>
2023-11-04 22:33:10 +0530

    add light mode in context api

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import myContext from './myContext';
 
 const MyState = (props) => {
-  const state = {
-    name: 'rajat',
-    rollNo: 33,
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'rgb(17, 24, 39)';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
   };
-  const color = 'red';
-  // when we pass more then 1 variable then
-  // value = {state, color} gives error
-  // correct method  value = {{state, color}} make an object
   return (
-    <myContext.Provider value={{ state, color }}>
+    <myContext.Provider value={{ mode, toggleMode }}>
       {props.children}
     </myContext.Provider>
   );
