b6187cd3c669d717aa0528503ddf6f5debca99f7
Rajat1ggc <Rajat1ggc@gmail.com>
2023-11-04 23:17:43 +0530

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Filter from '../../components/filter/Filter';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Layout from '../../components/layout/Layout';
 import ProductCard from '../../components/productCard/ProductCard';
+import Testimonial from '../../components/testimonial/Testimonial';
 import Track from '../../components/track/Track';
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
         <Filter />
         <ProductCard />
         <Track />
+        <Testimonial />
       </Layout>
     </div>
   );
