d9b15ce3881cfb97097a3631065321ed084c2ff1
Rajat Goyal <91239988+Rajat1Ggc@users.noreply.github.com>
2023-11-04 16:31:25 +0530

    react router setup (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Home from './pages/home/Home';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Home />} />
+          <Route path="/cart" element={<Home />} />
+          <Route path="/dashboard" element={<Home />} />
+          {/* when path not match then this path run  */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
