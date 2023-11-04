2f5d2bcc68673ff476cc5e687eb479d594702567
Rajat1ggc <Rajat1ggc@gmail.com>
2023-11-04 22:35:05 +0530

    remove context applid example

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,24 +1,9 @@
-import { useContext } from 'react';
 import Layout from '../../components/layout/Layout';
-import myContext from '../../context/data/myContext';
 
 const Home = () => {
-  const context = useContext(myContext);
-  console.log(context);
-  // output : {name: 'rajat', rollNo: 33}
-  // when single variable comes
-  // const { name, rollNo } = context;
-  // when more then 2 variable comes then destr
-  const { state, color } = context;
-
-  // destructred
   return (
     <div>
-      <Layout>
-        <h1>Name: {state.name}</h1>
-        <h1>roll no.: {state.rollNo}</h1>
-        <h1>color: {color}</h1>
-      </Layout>
+      <Layout>home</Layout>
     </div>
   );
 };
--- src/pages/order/Orders.jsx
+++ src/pages/order/Orders.jsx
@@ -1,17 +1,7 @@
-import { useContext } from 'react';
-import myContext from '../../context/data/myContext';
 import Layout from '../../components/layout/Layout';
 
 const Order = () => {
-  const context = useContext(myContext);
-  const { name, rollNo } = context;
-
-  return (
-    <Layout>
-      <h1>Name: {name}</h1>
-      <h1>Name: {rollNo}</h1>
-    </Layout>
-  );
+  return <Layout>order</Layout>;
 };
 
 export default Order;
