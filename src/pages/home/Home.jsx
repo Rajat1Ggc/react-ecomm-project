1670ee70334e94fa47bf4044ef68f57e00ec9745
Rajat1ggc <Rajat1ggc@gmail.com>
2023-11-04 23:00:44 +0530

    heroSection

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+import HeroSection from '../../components/heroSection/HeroSection';
 import Layout from '../../components/layout/Layout';
 
 const Home = () => {
   return (
     <div>
-      <Layout>home</Layout>
+      <Layout>
+        <HeroSection />
+      </Layout>
     </div>
   );
 };
