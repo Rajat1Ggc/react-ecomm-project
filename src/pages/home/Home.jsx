9d5591ea9bfa3b5c6c02c4f02f0fafb96fb92211
Rajat1ggc <Rajat1ggc@gmail.com>
2023-11-04 23:07:30 +0530

    ProductCard UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Filter from '../../components/filter/Filter';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Layout from '../../components/layout/Layout';
+import ProductCard from '../../components/productCard/ProductCard';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Layout>
         <HeroSection />
         <Filter />
+        <ProductCard />
       </Layout>
     </div>
   );
