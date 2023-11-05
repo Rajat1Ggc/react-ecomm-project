921c32a56d129c314c36c2ecccb5cf08115ce466
Rajat1ggc <Rajat1ggc@gmail.com>
2023-11-05 15:39:20 +0530

    add react toast and use

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Signup from './pages/registration/Signup';
 import ProductInfo from './pages/productInfo/ProductInfo';
 import UpdateProduct from './pages/admin/ pages/UpdateProduct';
 import AddProduct from './pages/admin/ pages/AddProduct';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -28,6 +30,7 @@ const App = () => {
           <Route path="/updateproduct" element={<UpdateProduct />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
+        <ToastContainer />
       </Router>
     </MyState>
   );
--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -1,6 +1,23 @@
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import myContext from '../../context/data/myContext';
+import { toast } from 'react-toastify';
 
 function Signup() {
+  const [name, setName] = useState('');
+  const [email, setemail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const context = useContext(myContext);
+  // const [loading, setloading] = context;
+
+  const signUp = () => {
+    console.log(name, email, password);
+    if (name === '' || email === '' || password === '') {
+      return toast.error('All fields must be fill ');
+    }
+  };
+
   return (
     <div className=" flex justify-center items-center h-screen">
       <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
@@ -11,7 +28,19 @@ function Signup() {
         </div>
         <div>
           <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
+            className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+            placeholder="Name"
+          />
+        </div>
+        <div>
+          <input
             type="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
             name="email"
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Email"
@@ -20,18 +49,23 @@ function Signup() {
         <div>
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Password"
           />
         </div>
         <div className=" flex justify-center mb-3">
-          <button className=" bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg">
+          <button
+            onClick={() => signUp()}
+            className=" bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg"
+          >
             Signup
           </button>
         </div>
         <div>
           <h2 className="text-white">
-            Have an account
+            Have an account?
             <Link className=" text-red-500 font-bold" to={'/login'}>
               Login
             </Link>
